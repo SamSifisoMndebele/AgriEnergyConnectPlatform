d4ee0896dff861ed5b2a8a66c9af80954af87142
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-07 23:55:18 +0200

    Add Farmers route to manage products

---
+++ Data/AgriEnergyConnectPlatformContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AgriEnergyConnectPlatform.Models;
+
+namespace AgriEnergyConnectPlatform.Data
+{
+    public class AgriEnergyConnectPlatformContext : DbContext
+    {
+        public AgriEnergyConnectPlatformContext (DbContextOptions<AgriEnergyConnectPlatformContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AgriEnergyConnectPlatform.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Pages/Farmers/Index.cshtml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AgriEnergyConnectPlatform.Data;
+using AgriEnergyConnectPlatform.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AgriEnergyConnectPlatform.Pages.Farmers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AgriEnergyConnectPlatform.Data.AgriEnergyConnectPlatformContext _context;
+
+        public IndexModel(AgriEnergyConnectPlatform.Data.AgriEnergyConnectPlatformContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+        
+        [BindProperty(SupportsGet = true)]
+        public string? Search { get; set; }
+
+        public SelectList? Categories { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Category { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<string> categoryQuery = from p in _context.Product
+                orderby p.Category
+                select p.Category;
+            
+            var products = from p in _context.Product 
+                select p;
+            
+            if (!string.IsNullOrEmpty(Category))
+            {
+                products = products.Where(p => p.Category == Category);
+            }
+            
+            if (!string.IsNullOrEmpty(Search))
+            {
+                products = products.Where(p => p.Name != null && p.Name.Contains(Search));
+            }
+            
+            Categories = new SelectList(await categoryQuery.Distinct().ToListAsync());
+            
+            Product = await products.ToListAsync();
+        }
+    }
+}
