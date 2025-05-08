27523ffc69db6aa7bbc57b466e36772d7bf04756
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-08 16:41:26 +0200

    Add initial migrations for Farmer and Product relationships
    This commit introduces migrations to establish the Farmer table and its relationship with the Product table. It also adjusts the FarmerId column in the Product table to be mandatory, enforcing referential integrity with a cascade delete.

---
+++ Migrations/20250508133027_new_migration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriEnergyConnectPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class new_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Farmer_FarmerId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FarmerId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Farmer_FarmerId",
+                table: "Product",
+                column: "FarmerId",
+                principalTable: "Farmer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Farmer_FarmerId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FarmerId",
+                table: "Product",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Farmer_FarmerId",
+                table: "Product",
+                column: "FarmerId",
+                principalTable: "Farmer",
+                principalColumn: "Id");
+        }
+    }
+}
