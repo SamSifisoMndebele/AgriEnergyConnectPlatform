3ed7bed9643401a68fa0e41e881db49566282898
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-10 09:26:44 +0200

    Add login functionality with cookie authentication
    Introduces a login page with form validation and cookie-based authentication. Includes `Login.cshtml`, `Login.cshtml.cs`, and supporting `CookieAuthentication` class for managing authentication scheme. Redirects authenticated users to the index page and retains proper security context.

---
+++ Pages/Auth/CookieAuthentication.cs
@@ -0,0 +1,35 @@
+﻿namespace AgriEnergyConnectPlatform.Pages.Auth;
+
+/// <summary>
+/// Default values related to cookie-based authentication handler
+/// </summary>
+public static class CookieAuthentication
+{
+    /// <summary>
+    /// The default value used for CookieAuthenticationOptions.AuthenticationScheme
+    /// </summary>
+    public const string AuthenticationScheme = "Cookies";
+    /// <summary>
+    /// The prefix used to provide a default CookieAuthenticationOptions.CookieName
+    /// </summary>
+    public static readonly string CookiePrefix = ".Auth.";
+    /// <summary>
+    /// The default value used by CookieAuthenticationMiddleware for the
+    /// CookieAuthenticationOptions.LoginPath
+    /// </summary>
+    public static readonly PathString LoginPath = new("/Auth/Login");
+    /// <summary>
+    /// The default value used by CookieAuthenticationMiddleware for the
+    /// CookieAuthenticationOptions.LogoutPath
+    /// </summary>
+    public static readonly PathString LogoutPath = new("/Auth/Logout");
+    /// <summary>
+    /// The default value used by CookieAuthenticationMiddleware for the
+    /// CookieAuthenticationOptions.AccessDeniedPath
+    /// </summary>
+    public static readonly PathString AccessDeniedPath = new("/Auth/AccessDenied");
+    /// <summary>
+    /// The default value of the CookieAuthenticationOptions.ReturnUrlParameter
+    /// </summary>
+    public static readonly string ReturnUrlParameter = "ReturnUrl";
+}
---
+++ Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgriEnergyConnectPlatform.Pages.Auth;
+
+public class Login : PageModel
+{
+    [BindProperty]
+    public Credential Credential { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!ModelState.IsValid) return Page();
+        
+        // Verify the user credentials
+        if (Credential is { Email: "user@mail.com", Password: "Password" })
+        {
+            // Create Security Context
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "user@mail.com"),
+                new Claim(ClaimTypes.Email, "user@mail.com"),
+                new Claim("FullName", "Sam Sifiso Mndebele"),
+                new Claim(ClaimTypes.Role, "Administrator"),
+            };
+            
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthentication.AuthenticationScheme);
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            
+            await HttpContext.SignInAsync(CookieAuthentication.AuthenticationScheme, claimsPrincipal);
+            
+            return RedirectToPage("/Index");
+        }
+
+        // Something failed. Redisplay the form.
+        return Page();
+    }
+}
+    
+public class Credential
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
