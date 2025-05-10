09759820ba8478d45aac1d193f30e3e35d0b0805
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-10 09:38:24 +0200

    Refactor login page and logic with improved structure.
    Moved the login page and model to the Auth directory for better organization. Enhanced authentication logic by introducing configurable properties for authentication tickets. Removed the old login implementation and replaced it with a cleaner structure.

--- Pages/Auth/Login.cshtml.cs
+++ Pages/Auth/Login.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
+using AgriEnergyConnectPlatform.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,7 @@ public class Login : PageModel
     public async Task<IActionResult> OnPost()
     {
         if (!ModelState.IsValid) return Page();
+        // ModelState.AddModelError(string.Empty, "Invalid login attempt.");
         
         // Verify the user credentials
         if (Credential is { Email: "user@mail.com", Password: "Password" })
@@ -34,11 +36,37 @@ public class Login : PageModel
             };
             
             var claimsIdentity = new ClaimsIdentity(claims, CookieAuthentication.AuthenticationScheme);
-            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                // The time at which the authentication ticket expires. A 
+                // value set here overrides the ExpireTimeSpan option of 
+                // CookieAuthenticationOptions set with AddCookie.
+
+                //IsPersistent = true,
+                // Whether the authentication session is persisted across 
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http 
+                // redirect response value.
+            };
             
-            await HttpContext.SignInAsync(CookieAuthentication.AuthenticationScheme, claimsPrincipal);
+            await HttpContext.SignInAsync(
+                CookieAuthentication.AuthenticationScheme, 
+                new ClaimsPrincipal(claimsIdentity), 
+                authProperties);
             
             return RedirectToPage("/Index");
+            // return LocalRedirect(Url.GetLocalUrl(returnUrl));
         }
 
         // Something failed. Redisplay the form.
