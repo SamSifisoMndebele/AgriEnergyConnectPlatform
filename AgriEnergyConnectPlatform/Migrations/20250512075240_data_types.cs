0443b0e9650e3ae1062a6986d4a8f0fe1ea3188c
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-05-12 09:54:36 +0200

    Update column data types in AppUser and Products tables
    Modified database schema to use more specific SQL data types for columns such as `char(36)` and `varchar`. Updated EF Core migration, model annotations, and snapshots to reflect these changes.

---
+++ AgriEnergyConnectPlatform/Migrations/20250512075240_data_types.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriEnergyConnectPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class data_types : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmerId",
+                table: "Products",
+                type: "char(36)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "AppUsers",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetAddress",
+                table: "AppUsers",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Province",
+                table: "AppUsers",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AppUsers",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 16);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Names",
+                table: "AppUsers",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AppUsers",
+                type: "varchar(256)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "AppUsers",
+                type: "char(36)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmerId",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(36)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "AppUsers",
+                type: "TEXT",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetAddress",
+                table: "AppUsers",
+                type: "TEXT",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Province",
+                table: "AppUsers",
+                type: "TEXT",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)",
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AppUsers",
+                type: "TEXT",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Names",
+                table: "AppUsers",
+                type: "TEXT",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AppUsers",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "AppUsers",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(36)");
+        }
+    }
+}
